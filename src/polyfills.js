aea43d8032f7d6239d6e05a681802af4ab3ad015
Colin Gourlay <Gourlay.Colin@abc.net.au>
2019-04-17 19:00:59 +1000

    Added String.prototype.startsWith polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -17,3 +17,12 @@
 
   window.CustomEvent = CustomEvent;
 })();
+
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+    value: function(search, pos) {
+      pos = !pos || pos < 0 ? 0 : +pos;
+      return this.substring(pos, pos + search.length) === search;
+    }
+  });
+}
